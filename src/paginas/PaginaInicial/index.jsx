f50214b428446c0628c984388e3db76eeb8c2fdc
Samuel Carvalho <samuel.carvalho.dev@gmail.com>
2023-01-31 21:28:02 -0300

    modulo 3

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,11 +1,18 @@
+import Banner from "../../components/Banner";
 import Cabecalho from "../../components/Cabecalho";
 import Menu from "../../components/Menu";
+import styles from './PaginaInicial.module.scss'
 
 export default function PaginaInicial () {
     return (
         <>
             <Cabecalho />
-            <Menu />
+            <main>
+                <section className={styles.principal}>
+                    <Menu />
+                    <Banner />
+                </section>
+            </main>
         </>
     )
 }
